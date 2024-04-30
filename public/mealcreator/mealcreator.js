fa94c2f1cacfdadc81a8afd7bed471815c075cee
MaikenGroenbech <maiken.2001@live.dk>
2024-04-30 12:56:00 +0200

    Tilføjelse til MealCreator - Modal, html, css

--- public/mealcreator/mealcreator.js
+++ public/mealcreator/mealcreator.js
@@ -1,17 +1,19 @@
-
+//Knapper til at åbne modal (tilføj måltid), åbne og lukke modalvindue.
 function openMealCreator() {
-    document.getElementById("overlay").style.display = "block";
+    document.getElementById("modal").style.display = "block";
     document.getElementById("mealCreatorModal").style.display = "block";
 }
 function closeMealCreator() {
-    document.getElementById("overlay").style.display = "none";
+    document.getElementById("modal").style.display = "none";
     document.getElementById("mealCreatorModal").style.display = "none";
 }
 
-function searchButton_MC(event) {
-    if (event.key === "Enter") {
+function searchButton_MC(event){
+    if(event.key === "Enter"){
         let text = document.getElementById("mcFoodSearch").value
         searchFoodMC(text);
 
     }
 }
+
+
